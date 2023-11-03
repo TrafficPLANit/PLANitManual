de01fcdc115c61ea66e0ba4b898d47ac92296f6c
markr <mark.raadsen@sydney.edu.au>
2023-11-03 12:13:37 +1100

    bring in sync with live website and v0.4.0

--- docs/python/getting-started/static_python/my_first_converter.py
+++ docs/python/getting-started/static_python/my_first_converter.py
@@ -9,12 +9,12 @@ osm_reader = network_converter.create_reader(NetworkReaderType.OSM, "Australia")
 osm_reader.settings.set_input_file(r"c:\Users\Public\sydneycbd_2023.osm.pbf")
 
 ################### SHAPE WRITER ########
-geo_writer = intermodal_converter.create_writer(IntermodalWriterType.SHAPE)
+geo_writer = network_converter.create_writer(NetworkWriterType.SHAPE)
 geo_writer.settings.set_output_directory(r"c:\Users\Public\")
 geo_writer.settings.set_country("Australia")
 # Explicitly set what to persist from the network (these flags are the default, but are included for illustration purposes)
-geo_writer.settings.network_settings.set_persist_nodes(True)
-geo_writer.settings.network_settings.set_persist_links(True)
+geo_writer.settings.set_persist_nodes(True)
+geo_writer.settings.set_persist_links(True)
 
 # perform conversion
 network_converter.convert(osm_reader, geo_writer)
